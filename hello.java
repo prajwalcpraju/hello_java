37d004be040fbccba1fdced3e3477e5fc8152734
PRAJWAL.C <88227526+prajwalcpraju@users.noreply.github.com>
2023-01-09 17:20:05 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
-public class hello{
-    public static void main(String[] args){
-        System.out.println("Hello_World");
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
 }
